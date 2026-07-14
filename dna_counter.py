623a9e3b425dca1fcc223c13beb22c7b977bbf12
radhvi-biotech <radhvimaheshwari2006@gmail.com>
2026-07-14 20:46:36 +0530

    counting of nucleotides

--- dna_counter.py
+++ dna_counter.py
@@ -7,3 +7,27 @@ print(dna)
 print("-----------")
 print("Sequence Length:")
 print(len(dna))
+count=0
+for nucleotide in dna:
+  if nucleotide== "A":
+    count=count+1
+print("A count:")
+print(count) 
+count=0 
+for nucleotide in dna:
+  if nucleotide== "C":
+    count=count+1 
+print("C count:")     
+print(count)
+count=0
+for nucleotide in dna:
+  if nucleotide== "G":
+    count=count+1
+print("G count:")     
+print(count)
+count=0  
+for nucleotide in dna:
+  if nucleotide== "T":
+    count=count+1
+print("T count:")   
+print(count)   
